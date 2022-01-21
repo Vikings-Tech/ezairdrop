94add0008c743dca0cce7f3071859833f4ddc655
Swarnab Garang <swarnabgarang@gmail.com>
2022-01-22 00:56:04 +0530

    reorganized directory structure

--- src/App.js
+++ src/App.js
@@ -9,27 +9,29 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { Logo } from './Logo';
 import Navbar from './components/Navbar';
-import { NotificationContainer, NotificationManager } from 'react-notifications';
+import {
+  NotificationContainer,
+  NotificationManager,
+} from 'react-notifications';
 import 'react-notifications/lib/notifications.css';
 import { Web3Provider } from './contexts/Web3Context';
-import { Web3Provider as ww } from "@ethersproject/providers";
-import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider as ww } from '@ethersproject/providers';
+import { Web3ReactProvider } from '@web3-react/core';
 import Airdrop from './pages/Airdrop/index';
 import Home from './pages/Home/index';
 import FAQ from './pages/FAQ/index';
 import Donate from './pages/Donate/index';
 import Footer from './components/Footer/index';
-import Erc20 from './pages/Airdrop/Erc20';
-
+import Erc20 from './pages/Airdrop/Tabs/Erc20';
 
 function getLibrary(provider) {
   var library;
 
-  if (provider?.chainType === "hmy") {
+  if (provider?.chainType === 'hmy') {
     library = provider.blockchain;
   } else {
     library = new ww(provider);
@@ -45,12 +47,10 @@ function App() {
       <NotificationContainer />
 
       <Router>
-
         <Web3ReactProvider getLibrary={getLibrary}>
-
           <Web3Provider>
             <Navbar />
-            <div style={{ minHeight: "90vh" }}>
+            <div style={{ minHeight: '90vh' }}>
               <Routes>
                 <Route path="/airdrop/erc20" element={<Erc20 />} />
                 <Route path="/airdrop" element={<Airdrop />} />
@@ -58,11 +58,9 @@ function App() {
                 <Route path="/donate" element={<Donate />} />
 
                 <Route path="/" element={<Home />} />
-
               </Routes>
             </div>
             <Footer />
-
           </Web3Provider>
         </Web3ReactProvider>
       </Router>
---
+++ src/pages/Airdrop/Tabs/Erc1155.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Center } from '@chakra-ui/react';
+
+function Erc1155() {
+  return (
+    <div>
+      <Center fontSize={30} mt={10}>
+        Coming soon!
+      </Center>
+    </div>
+  );
+}
+
+export default Erc1155;
